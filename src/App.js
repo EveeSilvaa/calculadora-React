2b27eab43d040b32c2b554daa7999d09803c1ca4
Evellyn Silva <evellynmaria593@gmail.com>
2024-11-14 21:29:16 -0300

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 
-function App() {
+import { Container, Content } from './styles';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+      olá
+      </Content>
+    </Container>
   );
 }
 
 export default App;
+//PAREI NAS PRIMEIRAS ESTILIZAÇÕES
